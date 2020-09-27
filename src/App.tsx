b1129b81f6863a9abf1987e29b301ce56d4826e0
Basia <b.kumor@yahoo.com>
2020-09-27 13:15:08 +0200

    fetch questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from './components/Card';
+import {fetchQuestions, Difficulty, QuestionState} from './API';
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [questionNum, setquestionNum] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [end, setEnd] = useState(false);
+
+  console.log(fetchQuestions(10, Difficulty.EASY));
 
   const startQuiz = async () => {
 
@@ -23,7 +34,14 @@ function App() {
         <p className="score">Score: </p>
       </header>
       <main>
-        <Card />
+        {/* <Card 
+          questionNum={questionNum} 
+          questionsTotal={TOTAL_QUESTIONS}
+          question={questions[questionNum].question}
+          answers={questions[questionNum].answers}
+          userAnswer={userAnswers ? userAnswers[questionNum] : null}
+          cb={checkAnswer}
+          /> */}
         <button className="next-btn" onClick={nextQuestion}>Next</button>
       </main>
     </div>
